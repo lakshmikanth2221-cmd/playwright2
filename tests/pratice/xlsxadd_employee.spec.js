904c696ee6f1c5368fc8c6baca521d6d80308ff2
playwright4 <lakshmikanth2221@gmail.com>
2026-07-05 13:32:24 +0530

    Add initial project structure with configuration files and test cases
    - Created .env file for environment variables
    - Added .gitignore to exclude unnecessary files
    - Initialized package.json and package-lock.json with dependencies
    - Implemented Playwright configuration in playwright.config.js
    - Added test data in login.json
    - Developed test cases for job titles, employee addition, employment status, pay grades, and work shifts
    - Created login tests for valid and invalid credentials
    - Added practice tests for logo verification and login functionality

---
+++ tests/pratice/xlsxadd_employee.spec.js
@@ -0,0 +1,67 @@
+
+
+import { test, expect } from '@playwright/test';
+import * as XLSX from 'xlsx';
+
+const workbook = XLSX.readFile('./testdata/employee.xlsx');
+
+const sheet = workbook.Sheets[workbook.SheetNames[0]];
+
+const employees = XLSX.utils.sheet_to_json(sheet);
+
+test('verify the add employee', async ({ page }) => {
+
+  await page.goto('/web/index.php/auth/login');
+
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
+
+  for (const employee of employees) {
+
+    await page.getByRole('link', { name: 'PIM' }).click();
+    await page.getByRole('link', { name: 'Add Employee' }).click();
+
+    await page.getByRole('textbox', { name: 'First Name' }).fill(employee["First Name"]);
+    await page.getByRole('textbox', { name: 'Last Name' }).fill(employee["Last Name"]);
+
+    await page.getByRole('button', { name: 'Save' }).click();
+
+    await expect(page.getByText('Success', { exact: true })).toBeVisible();
+  }
+
+});
+test('verify the add employee negitive testcasess in empty box', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  console.log("lacnch the appliction")
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  console.log("print the user name")
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  console.log("launch the password")
+  await page.getByRole('button', { name: 'Login' }).click();
+  console.log("click on the login button")
+  await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
+  console.log("user neviget dash bord page")
+  await page.getByRole('link', { name: 'PIM' }).click();
+  console.log("user show click on the pim button ")
+  await page.getByRole('link', { name: 'Add Employee' }).click();
+  await page.getByRole('button', { name: 'Save' }).click();
+  await expect(page.getByText('Required').first()).toBeVisible();
+  await expect(page.getByText('Required').nth(1)).toBeVisible();
+});
+test('verify the add employee negitive testcasess in more then 30 casess', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
+  await page.getByRole('link', { name: 'Admin' }).click();
+  await page.getByRole('link', { name: 'PIM' }).click();
+  await page.getByRole('link', { name: 'Add Employee' }).click();
+  await page.getByRole('textbox', { name: 'First Name' }).fill('ldhfoihjkmvjkpofndjghioufhfnkljbuohvvhi');
+  await page.getByRole('textbox', { name: 'Last Name' }).fill('d,mgnierohgjiur3y9fjncknlcbvug bifhihnjgjkjlch');
+  await expect(page.getByText('Should not exceed 30').first()).toBeVisible();
+  await expect(page.getByText('Should not exceed 30').nth(1)).toBeVisible();
+});
